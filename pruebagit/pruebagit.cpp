dfb5619a2734b9e25b248bdb75c4999b1b544995
Renzo Jesus Navarro Flores <110375272+Renzo-J-Navarro@users.noreply.github.com>
2025-09-17 22:05:32 -0500

    Agregacion menuInicio en el Main

--- pruebagit/pruebagit.cpp
+++ pruebagit/pruebagit.cpp
@@ -5,6 +5,17 @@ using namespace std;
 using namespace System;
 
 
+void menuInicio() {
+	cout << "Bienvenido al programa de gestion de empleados" << endl;
+	cout << "1. Agregar empleado" << endl;
+	cout << "2. Mostrar empleados" << endl;
+	cout << "3. Buscar empleado" << endl;
+	cout << "4. Eliminar empleado" << endl;
+	cout << "5. Salir" << endl;
+	cout << "Seleccione una opcion: ";
+}
+
+
 int main()
 {
     return 0;
